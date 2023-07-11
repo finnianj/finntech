ff60c3528fb6087d7c73a317ee27e956e0150cd7
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-07-11 12:19:01 +0200

    header image

--- pages/index.js
+++ pages/index.js
@@ -12,12 +12,9 @@ export default function Home() {
   return (
     <>
     <Navbar />
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
 
-    <header className={styles.header}>
+
+    <header className={styles.header} style={{height: 'calc(100vh - 80px)'}}>
       <div className={styles.headerLeft}>
         <h1 className={styles.headerTitle}>
           Empowering Your Journey
@@ -26,18 +23,13 @@ export default function Home() {
           At Finntech, we are committed to revolutionizing the way you manage your finances. Our cutting-edge platform offers a wide range of financial services designed to simplify your life and help you achieve your goals. Whether you're an individual looking to optimize your personal finances or a business owner seeking growth opportunities, we have the tools and expertise to support you every step of the way.
         </p>
       </div>
-        <Image
-          priority
-          src="/images/finance.jpeg"
-          className={styles.headerImage}
-          height={500}
-          width={500}
-          alt=""
-        />
-    
+
+      <div className={styles.headerRight}>
+      </div>
+
     </header>
 
-      <p>
+      {/* <p>
 
 
 At Finntech, we are committed to revolutionizing the way you manage your finances. Our cutting-edge platform offers a wide range of financial services designed to simplify your life and help you achieve your goals. Whether you're an individual looking to optimize your personal finances or a business owner seeking growth opportunities, we have the tools and expertise to support you every step of the way.
@@ -61,9 +53,9 @@ Financial Planning: Take the guesswork out of financial planning with our compre
 Join the Finntech Community Today
 
 Ready to experience the future of finance? Sign up for a free account and discover the endless possibilities that Finntech has to offer. Our dedicated team of financial experts is here to support you, providing unparalleled customer service and guidance along your financial journey. Don't miss out — start your adventure with Finntech today.
-      </p>
+      </p> */}
 
-    </Layout>
+
     </>
   );
 }

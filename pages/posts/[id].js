8e17b21c6878de02192dff9800114a28343a9b8d
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-07-19 12:40:21 +0200

    blog

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,6 @@
 import utilStyles from '../../styles/utils.module.css';
 import Head from 'next/head';
+import Image from 'next/image';
 import Navbar from '../../components/navbar'
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts';
@@ -32,6 +33,16 @@ export default function Post({ postData }) {
             <title>{postData.title}</title>
           </Head>
           <article>
+            <div className={utilStyles.container}>
+              <Image
+                priority
+                src="/images/profile.png"
+                className={utilStyles.borderCircle}
+                height={108}
+                width={108}
+                alt=""
+              />
+            </div>
             <h1 className={utilStyles.headingXl}>{postData.title}</h1>
             <div className={utilStyles.lightText}>
               <Date dateString={postData.date} />

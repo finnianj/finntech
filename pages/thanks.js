fdcbc32b861840be5d9c95b97a19095e904aa7fc
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-07-20 10:44:43 +0200

    separate pages

---
+++ pages/thanks.js
@@ -0,0 +1,9 @@
+import utilStyles from '../../styles/utils.module.css';
+import Head from 'next/head';
+import Image from 'next/image';
+import Navbar from '../../components/navbar'
+import Layout from '../../components/layout'
+
+export default function Thanks() {
+  
+}

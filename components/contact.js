cfe7e6224cfba4b0ae658945d33460a2628c133e
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-07-21 12:16:17 +0200

    favicon

--- components/contact.js
+++ components/contact.js
@@ -17,7 +17,7 @@ export default function Contact() {
             <br></br>
             <FontAwesomeIcon icon={faEnvelope} className={styles.faIcon}></FontAwesomeIcon>
           </h1>
-          <p className={utilStyles.lightText}>Please contact our sales team to receive more information on how to get started with Finntech</p>
+          <p className={utilStyles.lightText}>Our sales team are here to assist you with any additional queries you may have</p>
           <p className={utilStyles.lightText}><FontAwesomeIcon icon={faPhone} className={styles.faIcon}></FontAwesomeIcon>  +49 1573 6600 324</p>
         </div>
       </div>

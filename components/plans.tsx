52d76bbe48c4bfeb1d5e46f613120e0d51d87550
finn <charltonjones.fj@gmailcom>
2024-06-26 13:23:41 +0200

    chore: Update plans module styles and layout

--- components/plans.tsx
+++ components/plans.tsx
@@ -10,35 +10,37 @@ export default function Plans() {
     <>
       <Link href={"/get_started"} className={utilStyles.noUnderline}>
         <div className={styles.card}>
-          <h3>Standard</h3>
-          <p>
-          The Standard Account is your all-inclusive solution for everyday banking needs. With secure mobile and online banking, instant fund transfers, a globally accepted debit card, and round-the-clock customer support, managing your finances has never been easier.
-          </p>
-          <ul>
-            <li><FontAwesomeIcon icon={faCheckCircle} className={styles.faIcon}></FontAwesomeIcon>Secure mobile and online banking</li>
-            <li><FontAwesomeIcon icon={faCheckCircle} className={styles.faIcon}></FontAwesomeIcon>Globally acceted debit card</li>
-            <li><FontAwesomeIcon icon={faCheckCircle} className={styles.faIcon}></FontAwesomeIcon>Temporary Virtual until Physical arrives</li>
-            <li><FontAwesomeIcon icon={faCheckCircle} className={styles.faIcon}></FontAwesomeIcon>24/7 customer support</li>
-          </ul>
-          <div className={styles.cardLower}>
-            <div className={styles.cardFees}>
-              <h5>No Monthly Fee</h5>
-              <p><em>Monthly maintenance fee</em></p>
-            </div>
-            <div className={styles.cardFees}>
-              <h5>Free</h5>
-              <p><em>Standard transfers</em></p>
-            </div>
-            <div className={styles.cardFees}>
-              <h5>Free</h5>
-              <p><em>ATM withdrawals at partner networks</em></p>
-            </div>
-            <div className={styles.cardFees}>
-              <h5>2%</h5>
-              <p><em>Foreign transaction fee</em></p>
+
+            <h3>Standard</h3>
+            <p>
+            The Standard Account is your all-inclusive solution for everyday banking needs. With secure mobile and online banking, instant fund transfers, a globally accepted debit card, and round-the-clock customer support, managing your finances has never been easier.
+            </p>
+            <ul>
+              <li><FontAwesomeIcon icon={faCheckCircle} className={styles.faIcon}></FontAwesomeIcon>Secure mobile and online banking</li>
+              <li><FontAwesomeIcon icon={faCheckCircle} className={styles.faIcon}></FontAwesomeIcon>Globally acceted debit card</li>
+              <li><FontAwesomeIcon icon={faCheckCircle} className={styles.faIcon}></FontAwesomeIcon>Temporary Virtual until Physical arrives</li>
+              <li><FontAwesomeIcon icon={faCheckCircle} className={styles.faIcon}></FontAwesomeIcon>24/7 customer support</li>
+            </ul>
+            <div className={styles.cardLower}>
+              <div className={styles.cardFees}>
+                <h5>No Monthly Fee</h5>
+                <p><em>Monthly maintenance fee</em></p>
+              </div>
+              <div className={styles.cardFees}>
+                <h5>Free</h5>
+                <p><em>Standard transfers</em></p>
+              </div>
+              <div className={styles.cardFees}>
+                <h5>Free</h5>
+                <p><em>ATM withdrawals at partner networks</em></p>
+              </div>
+              <div className={styles.cardFees}>
+                <h5>2%</h5>
+                <p><em>Foreign transaction fee</em></p>
+              </div>
             </div>
           </div>
-        </div>
+
       </Link>
 
       <Link href={"/get_started"} className={utilStyles.noUnderline}>
